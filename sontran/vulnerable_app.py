cac646ed328f80418b0e28a85eb45d468492d0d4
tas1hc <son.tranduc@vn.bosch.com>
2023-05-23 11:28:30 +0700

    Update test

--- sontran/vulnerable_app.py
+++ sontran/vulnerable_app.py
@@ -1,67 +1,80 @@
-import os
-
-###############################
-# CWE-628: Function Call with Incorrectly Specified Arguments
-ADMIN_ROLES = 1
-USER_GRANTED_ROLES = 2
-def accessGranted(resource, user):
-    userRoles = getUserRoles(user)
-    return accessGranted(resource, ADMIN_ROLES)
-
-def getUserRoles(user):
-    if user == 1: return ADMIN_ROLES
-    if user == 2: return USER_GRANTED_ROLES
-    return 0
-
-def accessGranted(resource, userRoles):
-    # grant or deny access based on user roles
-    if (userRoles == ADMIN_ROLES):
-        print("Access granted for ADMIN")
-    elif (userRoles == USER_GRANTED_ROLES):
-        print("Access granted for USER-GRANTED-ROLES")
-    else:
-        print("Access denied")
+import os, subprocess, sqlite3
+import requests
+import time
+import math
 
 ###############################
 # CWE-1041: Use of Redundant Code
 # CWE-561: Dead Code
 # CWE-570: Expression is Always False
-def calculate_surface_area(s, r):
+def calculate_surface_area(s, r, d):
     pi = 3.14159
     surface_area = 0
     result = 0
     isSValid = False
     isRValid = False
 
-    if(r > 0.0 and s > 0.0):
-        isRValid = True
-        isSValid = True
-        surface_area = pi * r * s + pi * pow(r, 2)
-        if (not isSValid):
-            print("This is dead code !!!")
-
-    if(r > 1.0 and s > 0.0):
+    if(s > 2.0 and r > 0.0):
         isRValid = True
         isRValid = True # set wrong variable
-        surface_area = pi * r * s + pi * pow(r, 2)
+        surface_area = (pi * r * s + pi * pow(r, 2))/d
+        if (isRValid and isSValid):
+            print("This is dead code !!!")
+    elif(s > 0.0 and r > 1.0):
+        isRValid = True
+        isRValid = True # set wrong variable
+        surface_area = (pi * r * s + pi * pow(r, 2))/d
         if (isSValid):
             print("This is also dead code !!!")
     
     if (isRValid and isSValid):
+        print("This is also another dead code !!!")
         result = surface_area
 
     return result
 
 ###############################
 # CWE-732: Incorrect Permission Assignment for Critical Resource
-def create_some_file(filename):
+def create_some_file(filename, mode):
     f = open(filename, "w")
     f.write("Secret phase: YOLO")
     f.close()
+    os.chmod(filename, 0x777)
 
-    ### issue
-    #os.chmod(filename, 0x777)
+###############################
+# CWE-78: Improper Neutralization of Special Elements used in an OS Command
+# attack by inject command "--option_a --option_b; chmod 777 ./injection_command.sh; ./injection_command.sh"
+def execute_internal_script():
+    internal_script_name = "example_script.sh"
+    options = input("Enter a options to execute: ")
+    os.system(internal_script_name + " " + options)
 
-    ### no issue
-    mode = 0x777
-    os.chmod(filename, mode)    
+###############################
+# CWE-94: Improper Control of Generation of Code ('Code Injection')
+# attack by inject "__import__('subprocess').getoutput('rm -r *')"
+# CWE-190: Integer Overflow or Wraparound
+# overflow of exp()
+def calc_sum_of_exp_value():
+    sum = 0
+    numbers = eval(input("Enter a comma-separated list of numbers: "))
+    for num in numbers:
+        sum = sum + math.exp(num)
+    print(f"Sum of {numbers} = {sum}")
+
+###############################
+# CWE-89: Improper Neutralization of Special Elements used in an SQL Command
+def execute_user_query():
+    user_query = input("Enter a SQL query: ")
+    execute_sql_query("SELECT * FROM users WHERE username = '" + user_query + "';")
+
+###############################
+# CWE-22: Improper Limitation of a Pathname to a Restricted Directory (Path Traversal)
+# CWE-703: Improper Check or Handling of Exceptional Conditions
+def read_file():
+    try:
+        file_path = input("Enter the file path to read: ")
+        with open(file_path, "r") as file:
+            content = file.read()
+            print("File content:", content)
+    except:
+        pass
